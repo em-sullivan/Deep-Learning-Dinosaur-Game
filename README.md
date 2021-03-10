1c6076af38217f892144bd6fe931d988057fa914
Eric Sullivan <eric.sullivan@maine.edu>
2021-03-10 11:54:10 -0500

    Python game framework

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Deep-Learning-Dinosaur-Game
 COS 470 Semester project at the University of Maine. Training an AI to play the Dinosaur game using a neural network and python.
+
+## Requirements
+Python 3.86 (it is recommended to use a virtual enviroment).
+After the virtual enviroment is created, install the dependices with:
+
+    python -m pip install -r requirements.txt
---
+++ dinosaurGame.py
@@ -0,0 +1,54 @@
+'''
+Dinosaur Game
+'''
+
+import random
+import pygame
+import numpy as np
+from pygame.locals import *
+
+class App:
+
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 640, 400
+        self.clock = None
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        pass
+
+    def on_render(self):
+        # White Background
+        self._display_surf.fill((255, 255, 255))
+
+        # Update display
+        pygame.display.update()
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while(self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        
+        self.on_cleanup()
+
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
